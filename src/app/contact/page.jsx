89ce3189135e51df60f3a4ed62306e8448282b32
shivansh22agra <agrawalshivansh22@gmail.com>
2023-07-16 01:42:41 +0530

    contact page update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import styles from "./contact.module.css";
+import Image from "next/image";
+import { Button } from "@/components/button/Button";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Let's Keep in Touch</div>
+      <div className={styles.row}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <form className={styles.form}>
+            <input type="text" placeholder="name" className={styles.input} />
+            <input type="text" placeholder="email" className={styles.input} />
+            <textarea
+              className={styles.input}
+              placeholder="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <Button url="#" text="Send" />
+          </form>{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
