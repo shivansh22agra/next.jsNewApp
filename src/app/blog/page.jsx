f2e29e2ba6c9cbc6f4a257ef5d991b1d9b1e8672
shivansh22agra <agrawalshivansh22@gmail.com>
2023-07-17 02:34:56 +0530

    Blog update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "./blog.module.css";
+import Image from "next/image";
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className={styles.container}>
+      <Image
+        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+        // fill={true}
+        width={300}
+        height={200}
+        alt=""
+      />
+      <div>
+        <h1>Text</h1>
+        <p>desc</p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
