fcd6e768444a6eff405773a7e731272999198840
shivansh22agra <agrawalshivansh22@gmail.com>
2023-07-17 00:38:38 +0530

    portfolio category update

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "./category.module.css";
+import { items } from "./data";
+import { Button } from "@/components/button/Button";
+import Image from "next/image";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+const CategoryPage = ({ params }) => {
+  console.log(`__params ${params.category}`);
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {items.applications.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryPage;
