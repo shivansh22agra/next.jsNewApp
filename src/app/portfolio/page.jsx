c07194e6c9f628680434dd0be6f3bb29ca495a82
shivansh22agra <agrawalshivansh22@gmail.com>
2023-07-16 22:32:53 +0530

    portfolio update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./portfolio.module.css";
+import Link from "next/link";
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className={styles.container}>
+      <h1>Our Work</h1>
+      <h3>Choose a gallery</h3>
+      <div className={styles.itemContainer}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrationss</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Applicatin</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
